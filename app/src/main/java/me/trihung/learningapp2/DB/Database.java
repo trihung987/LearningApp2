84ebd198ed268cb31c586b5c156ad55edbece7ca
hungle10 <caylaco1981@gmail.com>
2025-04-15 15:44:25 +0700

    hung

--- app/src/main/java/me/trihung/learningapp2/DB/Database.java
+++ app/src/main/java/me/trihung/learningapp2/DB/Database.java
@@ -5,9 +5,44 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Database extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "LearningEnglish.db";
+    private final Context context;
+    private final String dbPath;
+
     public Database(Context context) {
-        super(context, "/data/data/com.example.myapplication/files/LearningEnglish.db", null, 1);
+        super(context, context.getFilesDir().getPath() + "/" + DB_NAME, null, 1);
+        this.context = context;
+        this.dbPath = context.getFilesDir().getPath() + "/" + DB_NAME;
+        copyDatabaseIfNeeded();
+    }
+
+    private void copyDatabaseIfNeeded() {
+        File dbFile = new File(dbPath);
+        if (!dbFile.exists()) {
+            try {
+                InputStream is = context.getAssets().open(DB_NAME);
+                OutputStream os = new FileOutputStream(dbFile);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, length);
+                }
+
+                os.flush();
+                os.close();
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void query_noresult(String sql){
@@ -20,15 +55,13 @@ public class Database extends SQLiteOpenHelper {
         return db.rawQuery(sql, null);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        // Không cần nếu có sẵn .db
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-
 }
